6c7e28cde7637353332614f728d748e2beb873da
yigitarsland <yigitarsland@icloud.com>
2024-03-30 16:20:20 +0100

    Fix: fix order of definitions

--- lab1.py
+++ lab1.py
@@ -1,42 +1,3 @@
-# Basis of the code
-def main():
-    database = "library.txt"
-    while True:
-        mode = input("Witam w Lublin Virtual Library, enter a command (add, modify, search, rm or q to quit): ").lower()
-        if mode == "q":
-            break
-        elif mode == 'add':
-            name = input('Enter book name: ')
-            author = input('Enter author name: ')
-            year = input('Enter published year: ')
-            genre = input('Enter genre: ')
-            bookInfo = [name, author, year, genre]
-            add(database, bookInfo)
-        elif mode == 'search':
-            query = input('Enter search query: ')
-            search(database, query)
-        elif mode == 'rm':
-            name = input('Enter book name to remove: ')
-            rm(database, name)
-        elif mode == 'modify':
-            oldName = input('Enter old book name (leave blank to skip): ')
-            oldAuthor = input('Enter old author name (leave blank to skip): ')
-            oldYear = input('Enter old published year (leave blank to skip): ')
-            oldGenre = input('Enter old genre (leave blank to skip): ')
-            oldInfo = [oldName, oldAuthor, oldYear, oldGenre]
-
-            newName = input('Enter new book name: ')
-            newAuthor = input('Enter new author name: ')
-            newYear = input('Enter new published year: ')
-            newGenre = input('Enter new genre: ')
-            newInfo = [newName, newAuthor, newYear, newGenre]
-
-            modify(database, oldInfo, newInfo)
-        else:
-            print('Invalid mode. Please try again.')
-
-if __name__ == "__main__":
-    main()
 
 # Add book
 def add(database, bookInfo):
@@ -93,3 +54,42 @@ def search(database, query):
     if not found:
         print("No matching books found.")
 
+# Basis of the code
+def main():
+    database = "library.txt"
+    while True:
+        mode = input("Witam w Lublin Virtual Library, enter a command (add, modify, search, rm or q to quit): ").lower()
+        if mode == "q":
+            break
+        elif mode == 'add':
+            name = input('Enter book name: ')
+            author = input('Enter author name: ')
+            year = input('Enter published year: ')
+            genre = input('Enter genre: ')
+            bookInfo = [name, author, year, genre]
+            add(database, bookInfo)
+        elif mode == 'search':
+            query = input('Enter search query: ')
+            search(database, query)
+        elif mode == 'rm':
+            name = input('Enter book name to remove: ')
+            rm(database, name)
+        elif mode == 'modify':
+            oldName = input('Enter old book name (leave blank to skip): ')
+            oldAuthor = input('Enter old author name (leave blank to skip): ')
+            oldYear = input('Enter old published year (leave blank to skip): ')
+            oldGenre = input('Enter old genre (leave blank to skip): ')
+            oldInfo = [oldName, oldAuthor, oldYear, oldGenre]
+
+            newName = input('Enter new book name: ')
+            newAuthor = input('Enter new author name: ')
+            newYear = input('Enter new published year: ')
+            newGenre = input('Enter new genre: ')
+            newInfo = [newName, newAuthor, newYear, newGenre]
+
+            modify(database, oldInfo, newInfo)
+        else:
+            print('Invalid mode. Please try again.')
+
+if __name__ == "__main__":
+    main()
