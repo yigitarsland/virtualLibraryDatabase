4215d51bc2fc6ce04a6f48a150d30a261f3655ec
yigitarsland <yigitarsland@icloud.com>
2024-04-01 11:50:57 +0200

    Feat: Add 'list()' and two minor features
    Feat: Add outputs to 'rm()' and 'search()' in case the book isn't available

--- lab1.py
+++ lab1.py
@@ -14,7 +14,9 @@ def rm(database, name):
             if name in line:
                 del line
                 file.write(lines)
-    print("Book removed successfully.")
+                print("Book removed successfully.")      
+            else:
+                print("Book not found.")
 
 # Modify book
 def modify(database, oldInfo=None, newInfo=None):
@@ -48,21 +50,31 @@ def search(database, query):
         if any(qElement in features for qElement in query):
             matched.append(', '.join(features).strip("[]".rstrip('\n')))
 
-    print("Matched books:")
-    for book in matched:
-        print(book)
+    if matched:
+        print("Matched books:")
+        for book in matched:
+            print(book)
+    else:
+        print("Book not found.")
 
 # List books
-#def list():
+def list(database):
+    with open(database, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            cleanLine = ''.join(line.strip('\n')).replace("'", '').replace('[', '').replace(']', '')
+            print(cleanLine)
+
  
 
 # Basis of the code
 def main():
     database = "library.txt"
     while True:
-        mode = input("Witam w Lublin Virtual Library, enter a command (add, modify, search, rm or q to quit): ").lower()
+        mode = input("Witam w Lublin Virtual Library, enter a command (add, rm, modify, search, list or q to quit): ").lower()
         if mode == "q":
             break
+
         elif mode == 'add':
             name = input('Enter book name: ')
             author = input('Enter author name: ')
@@ -70,12 +82,11 @@ def main():
             genre = input('Enter genre: ')
             bookInfo = [name, author, year, genre]
             add(database, bookInfo)
-        elif mode == 'search':
-            query = input('Enter search query: ')
-            search(database, query)
+
         elif mode == 'rm':
             name = input('Enter book name to remove: ')
             rm(database, name)
+
         elif mode == 'modify':
             oldName = input('Enter old book name (leave blank to skip): ')
             oldAuthor = input('Enter old author name (leave blank to skip): ')
@@ -93,6 +104,14 @@ def main():
                 newInfo = [newName, newAuthor, newYear, newGenre]
 
                 modify(database, oldInfo, newInfo)
+
+        elif mode == 'search':
+            query = input('Enter search query: ')
+            search(database, query)
+
+        elif mode == 'list':
+            list(database)
+
         else:
             print('Invalid mode. Please try again.')
 
